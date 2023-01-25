db759f6f43891114bc7d44f2a44a7a6fe4f3a5f8
VoiceOfSha <116538465+VoiceOfSha@users.noreply.github.com>
2023-01-25 08:09:48 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -4,9 +4,9 @@ class Config:
     API_ID = int(getenv("API_ID", "18466786"))
     API_HASH = getenv("API_HASH", "d2ce12c5c963cbb2210a669def0868aa")
     BOT_TOKEN = getenv("BOT_TOKEN", "5642909631:AAHKzZdDm5eYoFniQfO6IHc7Qqi1OPc9dAA")
-    FSUB = getenv("FSUB", "TamilanBotsZ")
+    FSUB = getenv("FSUB", "TamilanMoviesChat")
     CHID = int(getenv("CHID", "-1001512788880"))
-    SUDO = list(map(int, getenv("SUDO").split()))
+    SUDO = int(getenv("SUDO", "1893496887"))
     MONGO_URI = getenv("MONGO_URI", "mongodb+srv://AAB:AAB@cluster0.hiuufty.mongodb.net/?retryWrites=true&w=majority")
     
 cfg = Config()
