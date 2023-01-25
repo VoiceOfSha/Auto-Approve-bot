ddc10799952550cdf832e4eff34ad64a991f612b
VoiceOfSha <116538465+VoiceOfSha@users.noreply.github.com>
2023-01-25 08:13:59 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -5,7 +5,7 @@ class Config:
     API_HASH = getenv("API_HASH", "d2ce12c5c963cbb2210a669def0868aa")
     BOT_TOKEN = getenv("BOT_TOKEN", "5642909631:AAHKzZdDm5eYoFniQfO6IHc7Qqi1OPc9dAA")
     FSUB = getenv("FSUB", "TamilanMoviesChat")
-    CHID = int(getenv("CHID", "-1001512788880"))
+    CHID = int(getenv("CHID", "-1001674828759")
     SUDO = int(getenv("SUDO", "1893496887"))
     MONGO_URI = getenv("MONGO_URI", "mongodb+srv://AAB:AAB@cluster0.hiuufty.mongodb.net/?retryWrites=true&w=majority")
     
